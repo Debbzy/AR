a65041c74988dfce0942ee406699db0c5d9b04f7
Debbzy <trulyadebawibowo@gmail.com>
2024-11-23 15:06:30 +0700

    panduan belum jadi

--- Assets/Script/ImageCapture.cs
+++ Assets/Script/ImageCapture.cs
@@ -1,13 +1,15 @@
 using System;
+using System.IO;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.Android;
 
 public class ImageCapture : MonoBehaviour
 {
     [Header("Capture Setting")]
-    public string Path;
+    public string FolderName = "Menjelajah_Negara"; // Nama folder di direktori publik
     public int Resolution;
     string filename;
 
@@ -17,11 +19,38 @@ public class ImageCapture : MonoBehaviour
     [Header("After Capture Setting")]
     public UnityEvent AfterCaptureEvent;
 
+
     void WaitCapture()
     {
-        filename = Path + "/" + DateTime.Now.ToString("MM_dd_yyyy_h_mm_ss") + ".jpg";
+        string directoryPath = "/storage/emulated/0/DCIM/Screenshots/Menjelajah_Negara";
+
+        // Pastikan folder ada atau buat jika tidak ada
+        if (!Directory.Exists(directoryPath))
+        {
+            Directory.CreateDirectory(directoryPath);
+        }
+
+        filename = directoryPath + "/" + DateTime.Now.ToString("MM_dd_yyyy_h_mm_ss") + ".jpg";
         ScreenCapture.CaptureScreenshot(filename, Resolution);
-        Debug.Log(filename);
+        Debug.Log("Screenshot saved to: " + filename);
+    }
+
+    string GetSavePath()
+    {
+        string savePath;
+
+#if UNITY_ANDROID
+        savePath = Path.Combine(Application.persistentDataPath, FolderName);
+
+        // Untuk galeri Google Photos, simpan di folder Pictures atau DCIM
+        savePath = Path.Combine("/storage/emulated/0/DCIM/Screenshots", FolderName);
+#elif UNITY_IOS
+        savePath = Path.Combine(Application.persistentDataPath, FolderName);
+#else
+        savePath = Path.Combine(Application.dataPath, FolderName);
+#endif
+
+        return savePath;
     }
 
     void ShowObjects()
---
+++ Assets/Script/PermissionManager.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using UnityEngine.Android;
+
+public class PermissionManager : MonoBehaviour
+{
+    void Start()
+    {
+        // Periksa dan minta izin WRITE_EXTERNAL_STORAGE
+        if (!Permission.HasUserAuthorizedPermission(Permission.ExternalStorageWrite))
+        {
+            Permission.RequestUserPermission(Permission.ExternalStorageWrite);
+        }
+    }
+}
